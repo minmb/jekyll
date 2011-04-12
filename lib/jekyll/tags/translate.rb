90a93962d51082e5f2f2d0fdb7b8417175cd0899
Sylvester Keil <sylvester@keil.or.at>
2011-04-12 18:52:08 +0200

    Use site.translator instead of FastGettext
    Adds namespaced and pluralized translate tags

--- lib/jekyll/tags/translate.rb
+++ lib/jekyll/tags/translate.rb
@@ -1,27 +1,29 @@
-require 'fast_gettext'
-
-# require 'ruby-debug'
-# Debugger.start
-
-
 module Jekyll
   class TranslateTag < Liquid::Tag
-    
     def initialize(tag_name, text, tokens)
       super
       @text = text.strip
     end
 
     def render(context)
-      site = context.registers[:site]
-      page = context['page']
-      locales = File.expand_path(site.config['locales'] || '_locales')
+      context.registers[:site].translator._(@text)
+    end
+  end
 
-      FastGettext.add_text_domain('jekyll', :path => locales, :type => :yaml)
-      FastGettext.text_domain = 'jekyll'
-      FastGettext.locale = page && page['language'] || page['lang'] || 'en' 
+  class TranslatePluralizedTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @keys = text.strip.split(/,\s*/)
+    end
 
-      FastGettext::Translation::_(@text)
+    def render(context)
+      context.registers[:site].translator.n_(*@keys)
+    end
+  end
+
+  class TranslateNamespaceTag < TranslateTag
+    def render(context)
+      context.registers[:site].translator.s_(@text)
     end
   end
 end
@@ -29,3 +31,5 @@ end
 Liquid::Template.register_tag('_', Jekyll::TranslateTag)
 Liquid::Template.register_tag('t', Jekyll::TranslateTag)
 Liquid::Template.register_tag('translate', Jekyll::TranslateTag)
+Liquid::Template.register_tag('n_', Jekyll::TranslatePluralizedTag)
+Liquid::Template.register_tag('s_', Jekyll::TranslateNamespaceTag)
